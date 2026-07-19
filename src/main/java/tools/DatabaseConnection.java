6d7e1db62101dc179c4935f303b3b441f9aae1bb
tomsht97 <tomersh97@gmail.com>
2026-07-19 23:37:19 +0300

    feat: enable env-vars DB credentials

--- src/main/java/tools/DatabaseConnection.java
+++ src/main/java/tools/DatabaseConnection.java
@@ -44,20 +44,22 @@ public class DatabaseConnection {
     }
 
     private static String getDbUrl() {
-        // Environment variables override what's defined in the config file
-        // This feature is used for the Docker support
-        String hostOverride = System.getenv("DB_HOST");
-        String host = hostOverride != null ? hostOverride : YamlConfig.config.server.DB_HOST;
+        String host = getEnvironmentOrConfig("DB_HOST", YamlConfig.config.server.DB_HOST);
         String dbUrl = String.format(YamlConfig.config.server.DB_URL_FORMAT, host);
         return dbUrl;
+    }
+
+    private static String getEnvironmentOrConfig(String environmentVariable, String configValue) {
+        String environmentValue = System.getenv(environmentVariable);
+        return environmentValue != null ? environmentValue : configValue;
     }
 
     private static HikariConfig getConfig() {
         HikariConfig config = new HikariConfig();
 
         config.setJdbcUrl(getDbUrl());
-        config.setUsername(YamlConfig.config.server.DB_USER);
-        config.setPassword(YamlConfig.config.server.DB_PASS);
+        config.setUsername(getEnvironmentOrConfig("DB_USER", YamlConfig.config.server.DB_USER));
+        config.setPassword(getEnvironmentOrConfig("DB_PASS", YamlConfig.config.server.DB_PASS));
 
         final int initFailTimeoutSeconds = YamlConfig.config.server.INIT_CONNECTION_POOL_TIMEOUT;
         config.setInitializationFailTimeout(SECONDS.toMillis(initFailTimeoutSeconds));
